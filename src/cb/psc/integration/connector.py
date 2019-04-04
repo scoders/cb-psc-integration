e127f0b63b6b7186e58218ff98eccc4a0469a339
William Woodruff <william@trailofbits.com>
2019-04-04 15:21:02 -0400

    integration/connector: More fleshing out of the connector interface

--- src/cb/psc/integration/connector.py
+++ src/cb/psc/integration/connector.py
@@ -1,3 +1,5 @@
+from .database import AnalysisResult
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -5,16 +7,35 @@ logger.setLevel(logging.DEBUG)
 
 
 class Connector(object):
-    """docstring for Connector"""
+    __instance = None
 
     def __init__(self):
-        super(Connector, self).__init__()
+        if self.__class__.__instance:
+            raise ValueError(f"{self.__class__.__name__} is a singleton")
+        else:
+            self.__class__.__instance = self
+
+    @classmethod
+    def instance(cls):
+        if cls.__instance is None:
+            cls()
+        return cls.__instance
 
     @classmethod
     def connectors(cls):
-        for konnector in cls.__subclasses__:
-            yield konnector.instance
+        for konnector in cls.__subclasses__():
+            yield konnector.instance()
 
     @property
     def name(self):
-        return None
+        return self.__class__.__name__.lower()
+
+    def result(self, binary, **kwargs):
+        return AnalysisResult.create(
+            sha256=binary.sha256,
+            connector_name=self.name,
+            **kwargs,
+        )
+
+    def analyze(self, binary, stream):
+        logger.warning("analyze() called on top-level Connector")
