91e2eecf983add1f055feb9fe405316f8faf06ff
SS <sahil.suneja@trailofbits.com>
2019-09-11 12:44:39 -0400

    Taxii connector WIP

--- src/connectors/taxii/connector.py
+++ src/connectors/taxii/connector.py
@@ -4,8 +4,18 @@ import os
 from functools import lru_cache
 from pathlib import Path
 from cabby import create_client
-
+from datetime import datetime, timedelta
 from cb.psc.integration.connector import Connector, ConnectorConfig
+
+from cabby.constants import (
+    CB_STIX_XML_111, CB_CAP_11, CB_SMIME,
+    CB_STIX_XML_10, CB_STIX_XML_101, CB_STIX_XML_11, CB_XENC_122002)
+
+CB_STIX_XML_12 = 'urn:stix.mitre.org:xml:1.2'
+
+BINDING_CHOICES = [CB_STIX_XML_111, CB_CAP_11, CB_SMIME, CB_STIX_XML_12,
+                   CB_STIX_XML_10, CB_STIX_XML_101, CB_STIX_XML_11,
+                   CB_XENC_122002]
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -29,13 +39,33 @@ class TaxiiConfig(ConnectorConfig):
     ca_cert: str = None
     http_proxy_url: str = None
     https_proxy_url: str = None
+    reports_limit: int = 10000
 
+class FeedHelper():
+    def __init__(self, start_date_str, minutes_to_advance):
+        self.minutes_to_advance = minutes_to_advance
+        self.start_date = datetime.strptime(
+             start_date_str,"%Y-%m-%d %H:%M:%S").replace(tzinfo=TZ_UTC)
+        self.end_date = self.start_date + timedelta(minutes=self.minutes_to_advance)
+        self.done = False
+        self.now = datetime.utcnow().replace(tzinfo=TZ_UTC)
+        if self.end_date > self.now:
+            self.end_date = self.now
+
+
+    def advance(): 
 
 
 
 class TaxiiConnector(Connector):
     Config = TaxiiConfig
     name = "taxii"
+
+
+    def create_feed_helper(self):
+        self.feed_helper = FeedHelper(self.config.start_date,
+                                      self.config.minutes_to_advance)
+
 
     # TODO: add try and except
     def create_taxii_client(self):
@@ -83,21 +113,70 @@ class TaxiiConnector(Connector):
             logger.info(f"Collection Name: {collection.name}, Collection Type: {collection.type}")
         return collections
 
+
+    def poll_server(self, collection):
+        uri = self.create_uri(self.config.poll_path)
+        try:
+            logger.info(f"Polling Collection: {collection.name}")
+            content_blocks = client.poll(uri=uri,
+                                         collection_name=collection.name,
+                                         begin_date=self.feed_helper.start_date,
+                                         end_date=self.feed_helper.end_date,
+                                         content_bindings=BINDING_CHOICES)
+        except Exception as e:
+            logger.info(e.message)
+            content_blocks = []
+        return content_blocks
+
+
+    def parse_collection_content_block(self, block_content):
+        #TODO 
+
     
+    def parse_collection_content(self, content):
+        for block in content_blocks:
+            try:
+                self.parse_collection_content_block(block.content)
+             except Exception as e:
+                logger.info(e.message)
+                continue
+
+
+
     def import_collection(self, collection):
         reports = []
-        uri = self.create_uri(self.config.poll_path)
+        reports_limit = self.config.reports_limit
+
+        data_set = False
+        if collection.type == 'DATA_SET':
+            data_set = True
 
         while True:
             num_times_empty_content_blocks = 0
             try:
-                #TODO: resume
+                content_blocks = self.poll_server(collection)
+                self.parse_collection_content(content_blocks)
+                if len(reports) > reports_limit:
+                    logger.info("We have reached the reports limit of {reports_limit}")
+                    break
+
             except Exception as e:
                 logger.info(traceback.format_exc())
             
-        data_set = False
-        if collection.type == 'DATA_SET':
-            data_set = True
+            if data_set: # data is unordered
+                break 
+            
+            if self.feed_helper.advance():
+                continue
+            else:
+                break
+
+        if len(reports) > reports_limit
+            logger.info("Truncating reports to length {reports_limit}")
+            reports = reports[:reports_limit]
+
+        return reports
+            
 
         
 
@@ -121,14 +200,16 @@ class TaxiiConnector(Connector):
     def analyze(self, binary, data):   # TODO:ignoring binary for now
         results = []
     
+        self.create_feed_helper()
+
         self.create_taxii_client()  # TODO: make this part of init, not analyze
                                     # assuming analyze is called repeatedly
-
+        
         available_collections = self.query_collections()
         if len(available_collections) == 0:
             logger.info('Unable to find any collections.  Exiting...')
             return results
 
-        self.import_collections(available_collections)
+        results = self.import_collections(available_collections)
 
         return results
