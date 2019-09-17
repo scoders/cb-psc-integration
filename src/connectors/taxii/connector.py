175015efd2cc3f8af1ac81e77c6fef1e23c76694
SS <sahil.suneja@trailofbits.com>
2019-09-17 14:45:44 -0400

    functional checkpoint

--- src/connectors/taxii/connector.py
+++ src/connectors/taxii/connector.py
@@ -1,10 +1,8 @@
 import logging
-import traceback
-from functools import lru_cache
 from datetime import datetime
 from cabby import create_client
-from connectors.taxii.stix_parse import sanitize_stix, parse_stix, BINDING_CHOICES
-from connectors.taxii.feed_helper import FeedHelper,UTC
+from connectors.taxii.stix_parse import parse_stix, BINDING_CHOICES
+from connectors.taxii.feed_helper import FeedHelper
 from cb.psc.integration.connector import Connector, ConnectorConfig
 
 log = logging.getLogger(__name__)
@@ -30,15 +28,13 @@ class TaxiiConfig(ConnectorConfig):
     http_proxy_url: str = None
     https_proxy_url: str = None
     reports_limit: int = 10000
-    fail_limit: int = 10   # num attempts per collection for 
-                           # polling server and parsing stix content
+    fail_limit: int = 10   # num attempts per collection for polling & parsing
+
 
 class TaxiiConnector(Connector):
     Config = TaxiiConfig
     name = "taxii"
 
-
-    # TODO: better exception handling
     def create_taxii_client(self):
         self.client = None
         try:
@@ -52,7 +48,7 @@ class TaxiiConnector(Connector):
                             ca_cert=conf.ca_cert,
                             cert_file=conf.cert_file,
                             key_file=conf.key_file)
-        
+
             proxy_dict = dict()
             if conf.http_proxy_url:
                 proxy_dict['http'] = conf.http_proxy_url
@@ -60,12 +56,11 @@ class TaxiiConnector(Connector):
                 proxy_dict['https'] = conf.https_proxy_url
             if proxy_dict:
                 client.set_proxies(proxy_dict)
-            
+
             self.client = client
 
         except Exception as e:
-            log.info(e.message)
-
+            log.error(f"Error creating client: {e}")
 
     def create_uri(self, config_path):
         uri = None
@@ -75,90 +70,77 @@ class TaxiiConnector(Connector):
             else:
                 uri = 'http://'
             uri = uri + self.config.site + config_path
-        return uri        
+        return uri
 
-
-    # TODO: better exception handling
     def query_collections(self):
         collections = []
         try:
             uri = self.create_uri(self.config.collection_management_path)
-            collections = self.client.get_collections(uri=uri) # autodetect if uri=None
+            collections = self.client.get_collections(
+                uri=uri)  # autodetect if uri=None
             for collection in collections:
-                log.info(f"Collection Name: {collection.name}, Collection Type: {collection.type}")
+                log.debug(f"Collection: {collection.name}, {collection.type}")
         except Exception as e:
-            log.info(e)
+            log.warning(f"Problem fetching collections from taxii server: {e}")
         return collections
-
 
     def poll_server(self, collection, feed_helper):
         content_blocks = []
         uri = self.create_uri(self.config.poll_path)
         try:
             log.info(f"Polling Collection: {collection.name}")
-            content_blocks = self.client.poll(uri=uri,
-                                         collection_name=collection.name,
-                                         #begin_date=self.config.start_date.replace(tzinfo=UTC()),
-                                         #end_date=None,
-                                         begin_date=feed_helper.start_date,
-                                         end_date=feed_helper.end_date,
-                                         content_bindings=BINDING_CHOICES)
+            content_blocks = self.client.poll(
+                                          uri=uri,
+                                          collection_name=collection.name,
+                                          begin_date=feed_helper.start_date,
+                                          end_date=feed_helper.end_date,
+                                          content_bindings=BINDING_CHOICES)
         except Exception as e:
-            log.warning(f"problem polling taxii server: {e.message}")
+            log.warning(f"problem polling taxii server: {e}")
         return content_blocks
-
 
     def parse_collection_content(self, content_blocks):
         reports = []
         for block in content_blocks:
-            log.debug(f"got content_block: {block}")
-            report = parse_stix(block.content, self.config.default_score)
-            reports.extend(report)
-            log.debug(f"got report:{report}")
-            if report:
-                log.debug(f"breaking")
-                break   #TODO: remove
+            reports.extend(
+                parse_stix(block.content, self.config.default_score))
         return reports
 
-
     def import_collection(self, collection):
-        num_fail = 0
+        num_times_empty_content_blocks = 0
         reports = []
         reports_limit = self.config.reports_limit
         feed_helper = FeedHelper(self.config.start_date,
                                  self.config.minutes_to_advance)
 
-        while feed_helper.advance():  
-            content_blocks = self.poll_server(collection, feed_helper)  
-            _reports = self.parse_collection_content(content_blocks)
-            if not _reports:
-                num_fail += 1
-            else:
-                reports.extend(_reports)
-                break   #TODO: remove
+        while feed_helper.advance():
+            content_blocks = self.poll_server(collection, feed_helper)
+            reports.extend(self.parse_collection_content(content_blocks))
+            if not reports:
+                num_times_empty_content_blocks += 1
             if len(reports) > reports_limit:
-                log.info("We have reached the reports limit of {reports_limit}")
+                log.info(
+                    "We have reached the reports limit of {reports_limit}")
                 break
-            if collection.type == 'DATA_SET': # data is unordered, not a feed
-                log.info(f"collection:{collection}; type data_set, not advancing feed")
-                break 
-            if num_fail > self.config.fail_limit:   # to prevent infinite loop
+            if collection.type == 'DATA_SET':  # data is unordered, not a feed
+                log.info(f"collection:{collection}; type data_set; breaking")
+                break
+            if num_times_empty_content_blocks > self.config.fail_limit:
                 log.error('Max fail limit reached; Exiting.')
-                break            
+                break
 
         if len(reports) > reports_limit:
             log.info("Truncating reports to length {reports_limit}")
             reports = reports[:reports_limit]
         return reports
-            
 
     def import_collections(self, available_collections):
         desired_collections = self.config.collections
-        desired_collections = [x.strip() for x in desired_collections.lower().split(',')]
-        
+        desired_collections = [x.strip()
+                               for x in desired_collections.lower().split(',')]
+
         want_all = False
         if '*' in desired_collections:
-            log.debug('desired collections: *')
             want_all = True
 
         reports = []
@@ -167,21 +149,18 @@ class TaxiiConnector(Connector):
                 log.debug(f"collection:{collection}; type not feed or data")
                 continue
             if not collection.available:
-                log.debug(f"collection:{collection}; not available")
+                log.debug(f"collection:{collection} not available")
                 continue
             if want_all or collection.name.lower() in desired_collections:
-                report = self.import_collection(collection)
-                reports.extend(report)
-                if report:
-                    break  #TODO: remove
+                reports.extend(self.import_collection(collection))
 
         return reports
-       
-    
-    def format_report(self, binary, report): 
+
+    def format_report(self, binary, report):
         try:
-            # XXX: we wasted report['description'] and report['title'] ith this interface
-            # NOTE: analysis_name = f"{report['title']};{report['id']}"  becomes > 64 len
+            # NOTE:
+            # report['description'] & report['title'] lost with this interface
+            # analysis_name = f"{report['title']};{report['id']}"  > 64 len
             analysis_name = report['id']
             scan_time = datetime.fromtimestamp(report['timestamp'])
             score = report['score']
@@ -194,29 +173,32 @@ class TaxiiConnector(Connector):
             for ioc_key, ioc_val in ioc_dict.items():
                 result.ioc(values=ioc_val, field=ioc_key, link=link)
         except Exception as e:
-            log.warning(e)
-            result = self.result(binary, analysis_name="exception_format_report", error=True)
+            log.warning(f"Problem in report formatting: {e}")
+            result = self.result(
+                binary, analysis_name="exception_format_report", error=True)
         return result
 
-
     def analyze(self, binary, data):   # TODO:ignoring binary for now
-        self.create_taxii_client()  # TODO: make this part of init, not analyze
-                                    # assuming analyze is called repeatedly
+        self.create_taxii_client()
+
         if not self.client:
             log.error('Unable to create taxii client.  Exiting...')
-            return [self.result(binary, analysis_name="exception_taxii_client", error=True)]
-        
+            return [self.result(binary,
+                                analysis_name="exception_taxii_client",
+                                error=True)]
+
         available_collections = self.query_collections()
         if not available_collections:
             log.warning('Unable to find any collections.  Exiting...')
-            return [self.result(binary, analysis_name="exception_query_collections", error=True)]
+            return [self.result(binary,
+                                analysis_name="exception_query_collections",
+                                error=True)]
 
         reports = self.import_collections(available_collections)
         if not reports:
             log.warning('Unable to import collections.  Exiting...')
-            return [self.result(binary, analysis_name="exception_import_collections", error=True)]
-            
-        results = []
-        for report in reports:
-            results.append(self.format_report(binary, report))
-        return results
+            return [self.result(binary,
+                                analysis_name="exception_import_collections",
+                                error=True)]
+
+        return [self.format_report(binary, report) for report in reports]
